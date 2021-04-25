3bfa2f59a5885dd7147948d40aec36e276c3498e
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-04-25 12:27:01 +0545

    Change version to 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="1337x", 
-    version="0.0.0",
+    version="0.0.1",
     author="Hemanta Pokharel",
     author_email="hemantapkh@yahoo.com",
     description="Unofficial API of 1337x.to",
